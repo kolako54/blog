099abe9eddb0e4f7f18690dcd3c5ec931af5e669
Elio <parhamwolf051@gmail.com>
2023-02-13 23:59:01 +0330

    fix: fix tiny bug

---
+++ components/common/AdminNav.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface IAppProps {
+}
+
+const App: React.FunctionComponent<IAppProps> = (props) => {
+    return (
+        <nav className='h-screen w-60'></nav>
+  ) ;
+};
+
+export default App;
